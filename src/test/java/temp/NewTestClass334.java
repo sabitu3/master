a37350bc9a5dcf3da7a87cb13e2ef9014aa2e610
sabitu3 <sabitu3@gmail.com>
2024-09-12 20:04:59 +0530

    commit test

---
+++ src/test/java/temp/NewTestClass334.java
@@ -0,0 +1,27 @@
+package temp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class NewTestClass334 {
+
+    String xpath = "//span/a[text()='Services']";
+    String items="//a[contains(@class,'top-navigation__item')]";
+    WebDriver driver;
+    public NewTestClass334(WebDriver d){
+        this.driver=d;
+
+    }
+
+
+    public WebElement clickServices(){
+        return driver.findElement(By.xpath(xpath));
+    }
+
+    public List<WebElement> findTopNavItems(){
+        return driver.findElements(By.xpath(items));
+    }
+}
